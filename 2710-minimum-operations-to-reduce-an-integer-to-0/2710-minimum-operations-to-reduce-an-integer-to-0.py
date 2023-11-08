504a6f7f0b3a5d6fac5afa86a025ceaf62b7f8fb
daely178 <70620782+daely178@users.noreply.github.com>
2023-11-07 22:22:06 -0800

    Time: 44 ms (30.44%), Space: 16.2 MB (74.67%) - LeetHub

--- 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.py
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.py
@@ -1,20 +1,21 @@
 class Solution:
     def minOperations(self, n: int) -> int:
-        
         ans = 0
 
         while n:
- 
-            if n&3 == 3:
-                n += 1
+            if n&3 == 3: # 3= bin(11)
                 ans += 1
+                n += 1
+
             elif n&1:
+                ans += 1
                 n -= 1
-                ans += 1                
+
             else:
-                n >>= 1
-        
+                n = n >> 1
+
         return ans
+            
 
 '''
     39 
@@ -32,20 +33,50 @@ class Solution:
     1 2  4   8    16 32 64 128 256 512 1024
 bin 1 10 100 1000
 
+    39
     0x27
     0010 0111 -> 0010 1000
     add 1 subtract 1000, subtract 10
 
     simple examples
-    11
-        add 1 subtract 100
-        subtract 1 subtract 10
+
+    1111 (15)
+    1 0000 -> -16
+
+    11(3)
+        add 1 subtract 100 (2^2)
+        subtract 1 subtract 10 (2)
     10
         subtract 10
     
     >> (right shift)
+        / 2
+        1000
+         100
+           1
+        32
+        10000
+        1-1
         no need to count because add or subtract
 
+    
+    39
+    0x27
+    0010 0111
 
+    continuous 1 (두자리만)
+        +1
+        count +=1
+        0010 1
 
+    shift 3 번
+        0000 0101
+
+        -1
+        count += 1
+        0000 0100
+
+        1
+        -1
+        count += 1
 '''        
