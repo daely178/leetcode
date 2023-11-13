6fab605a6640b239e034141bbaa02148c14abaf1
daely178 <70620782+daely178@users.noreply.github.com>
2023-11-12 20:55:49 -0800

    Time: 244 ms (11.3%), Space: 17.9 MB (44.63%) - LeetHub

--- 3017-number-of-beautiful-integers-in-the-range/3017-number-of-beautiful-integers-in-the-range.py
+++ 3017-number-of-beautiful-integers-in-the-range/3017-number-of-beautiful-integers-in-the-range.py
@@ -1,5 +1,50 @@
 class Solution:
     def numberOfBeautifulIntegers(self, low: int, high: int, k: int) -> int:
+        memo = {}
+        digits = []
+        
+        def solve(pos, odd, even, rem, flag, lead_zero):
+            if odd > len(digits) or even > len(digits):
+                return 0
+            if pos < 0:
+                return int(odd == even and rem == 0)
+
+            inputs = (pos, odd, even, rem, flag, lead_zero)
+            if inputs in memo:
+                return memo[inputs]
+
+            answer = 0
+            for digit in range(digits[pos] + 1 if flag else 10):
+                new_odd = odd
+                new_even = even
+                if not lead_zero or digit > 0:
+                    if digit % 2 == 1: 
+                        new_odd += 1
+                    else: 
+                        new_even += 1
+                new_rem = (rem * 10 + digit) % k
+                new_flag = flag and (digit == digits[pos])
+                new_lead_zero = lead_zero and (digit == 0)
+                answer += solve(pos-1, new_odd, new_even, new_rem, new_flag, new_lead_zero)
+
+            memo[inputs] = answer
+            return answer
+
+        def process(n):
+            memo.clear()
+            digits.clear()
+
+            while n > 0:
+                digits.append(n % 10)
+                n //= 10
+
+            return solve(len(digits)-1, 0, 0, 0, True, True)
+
+        return process(high) - process(low-1)
+        
+        
+class Solution2:
+    def numberOfBeautifulIntegers(self, low: int, high: int, k: int) -> int:
         
         @cache
         def dp(i, flag, rest, odd_even):
@@ -29,7 +74,7 @@ class Solution:
         t = str(low - 1)
         l = dp(0, 0, 0, 0)
         return h - l
-        
+
 class SolutionBruteforce:
     def numberOfBeautifulIntegers(self, low: int, high: int, k: int) -> int:
         
