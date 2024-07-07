15f261c8ed369e52a893f211d03097c2ea737d07
daely178 <70620782+daely178@users.noreply.github.com>
2024-07-07 16:32:48 -0700

    Time: 47 ms (77.54%), Space: 16.5 MB (40.57%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,34 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         
-        q = deque()
-        oranges = 0
+        rq = deque()
+        fresh_oranges = 0
         rows, cols = len(grid), len(grid[0])
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
-                    q.append((r,c))
+                    rq.append((r,c))
                 elif grid[r][c] == 1:
-                    oranges += 1
-        
-        q.append((-1, -1))
+                    fresh_oranges += 1
 
-        minutes_elapsed = -1
+        minutes_elapsed = 0
         directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        while q:
-            r,c = q.popleft()
-            if r == -1:
-                minutes_elapsed +=1
-                if q:
-                    q.append((-1,-1))
-            else:
+        while rq and fresh_oranges:
+            rq_size = len(rq)    
+            minutes_elapsed += 1
+            for _ in range(rq_size):
+                r,c = rq.popleft()
                 for d in directions:
                     nr,nc = r+d[0], c+d[1]
                     if rows > nr >= 0 and cols > nc >= 0 and grid[nr][nc] == 1:
                         grid[nr][nc] = 2
-                        oranges -= 1
-                        q.append((nr, nc))
-        return minutes_elapsed if oranges == 0 else -1
+                        fresh_oranges -= 1
+                        rq.append((nr, nc))
+
+        return minutes_elapsed if fresh_oranges == 0 else -1
 
 # [[2,1,1],
 #. [1,1,0],
