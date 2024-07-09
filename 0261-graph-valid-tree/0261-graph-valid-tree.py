3c46ad4d7ebea17c96b54019e1a6e56449db1fc4
daely178 <70620782+daely178@users.noreply.github.com>
2024-07-08 23:54:10 -0700

    Time: 89 ms (41.82%), Space: 18.3 MB (45.72%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,16 +1,13 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         
-        par = {}
+        par = [-1 for _ in range(n)]
         
         def find(node):
-            if node not in par:
-                par[node] = node
-
-            while par[node] != node:
-                node = par[node]
+            if par[node] == -1:
+                return node
             
-            return node
+            return find(par[node])
 
         for edge in edges:
             u,v = find(edge[0]), find(edge[1])
