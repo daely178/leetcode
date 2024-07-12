5eb5a5b74df64b9c1228818a60c766b0355b5f6b
daely178 <70620782+daely178@users.noreply.github.com>
2024-07-11 23:05:36 -0700

    Time: 34 ms (72.12%), Space: 16.5 MB (64.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -5,7 +5,7 @@ class Solution:
         for val in nums:
             best = max(val+prevprev, prev)
             prev,prevprev = best, prev
-        return max(prev, prevprev)
+        return prev
 
 '''
     constrains : alert if robbing adjacent houses
@@ -14,4 +14,8 @@ class Solution:
     [1,2,3,1] 1+3
 
     [2,7,9,3,1] 2+9+1
+
+    formula 
+    dp = max(dp[i-1], dp[i-2]+nums[i]
+
 '''
