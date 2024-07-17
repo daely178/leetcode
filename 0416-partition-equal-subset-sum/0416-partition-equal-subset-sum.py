75b8c7c11e8596e6c605edda2b104279803fc4e4
daely178 <70620782+daely178@users.noreply.github.com>
2024-07-16 23:36:51 -0700

    Time: 552 ms (62.19%), Space: 16.6 MB (88.99%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,26 +1,40 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        
-        total_sum = sum(nums)
-        if total_sum % 2 != 0:
+                
+        if sum(nums)&1:
             return False
         
-        subset_sum = total_sum//2
-        n = len(nums)
+        total = sum(nums)//2
+        dp = [True]+[False]*total
         
-        dp = [[False] * (subset_sum+1) for _ in range(n+1)]
-        dp[0][0] = True
-
-        for i in range(1, n+1):
-            curr = nums[i-1]
-            for j in range(subset_sum+1):
-                if j< curr:
-                    dp[i][j] = dp[i-1][j]
-                else:
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j-curr]
-        return dp[n][subset_sum]
+        for num in nums:
+            for curr in range(total, num-1, -1):
+                dp[curr] = dp[curr] or dp[curr-num]
+        return dp[total]
 
 '''
     brute force
+    half_sum = sum//2 # 2 partitions sum is same as total sum
     
+    nums = [1,5,11,5]
+    half_sum = 22/2 = 11
+
+    tree
+                    11 : 1,5,11,5
+
+                    select or not select current item
+
+        5                        0
+     16.      5           11           0
+         10.    5        16.  11.   5.     0
+        11 10. 6.  5              6.   5 1.   0
+
+    11 : 1,5,11,5
+dp
+    0 1 2 3 4
+  0 T F F F F
+  1 F F F F F
+  2 F F F F F
+  3 F F F F F
+
 '''        
