04140d7e84fd12358e8db7bf489c5e73dfed4b47
daely178 <70620782+daely178@users.noreply.github.com>
2024-07-24 19:37:05 -0700

    Time: 29 ms (91.73%), Space: 16.7 MB (19.85%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -4,24 +4,19 @@ class Solution:
         if s=="":
             return True
 
-        id = 0
-        while id<len(t) and t[id] != s[0]:
-            id += 1
-        if id == len(t):
-            return False
 
-        if t[id] == s[0]: #s = "abc"         id = 0
-            sId = 1
-            matchCnt = 1
-            id2 = id + 1
+        sId = 0
+        tId = 0
+        matchCnt = 0
 
-            while id2<len(t) and sId<len(s):
-                if t[id2] == s[sId]: # c == c
-                    matchCnt+=1 # 3
-                    sId+=1  # 3 
-                id2 += 1
-            if matchCnt == len(s): # 3==3
-                return True
+        while tId<len(t) and sId<len(s):
+
+            if t[tId] == s[sId]: # c == c
+                sId+=1  # 3 
+
+                if sId == len(s): # 3==3
+                    return True                
+            tId += 1
 
         return False
         
