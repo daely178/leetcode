81ac708ab8e7eb96adb70d8f6aea9190eb9bb8fe
daely178 <70620782+daely178@users.noreply.github.com>
2024-08-08 19:32:00 -0700

    Time: 60 ms (98.3%), Space: 18.7 MB (79.19%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        @cache
+        def min_distance(i1, i2):
+            if not i1 or not i2:
+                return max(i1,i2)
+            
+            return (
+                min_distance(i1-1, i2-1)
+                if word1[i1-1] == word2[i2-1]
+                else 1+ min(
+                    min_distance(i1-1, i2-1),
+                    min_distance(i1, i2-1),
+                    min_distance(i1-1, i2),
+                )
+            )
+
+        return min_distance(len(word1), len(word2))
