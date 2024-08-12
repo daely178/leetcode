1dcd60edbefa3b6d7f09a7fe0c9291d5157f6ffe
daely178 <70620782+daely178@users.noreply.github.com>
2024-08-11 21:47:14 -0700

    Time: 41 ms (25.01%), Space: 16.5 MB (79.05%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = 0
+        for i in range(32):
+            if n&(1<<i):
+                ans |= (1<<(31-i))
+        return ans
+
+'''
+    00000010100101000001111010011100
+
+    00111001011110000010100101000000
+'''                
