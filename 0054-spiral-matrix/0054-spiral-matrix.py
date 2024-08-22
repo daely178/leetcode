bf526274802b17aed6bc598eaef1ea81ff083807
daely178 <70620782+daely178@users.noreply.github.com>
2024-08-22 00:19:23 -0700

    Time: 44 ms (7.47%), Space: 16.3 MB (99.81%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -3,47 +3,23 @@ class Solution:
         
         n = len(matrix)
         m = len(matrix[0])
-        UP,DOWN,RIGHT,LEFT = 0,1,2,3
         ans = []
-        visited = [[False for _ in range(m)] for _ in range(n)]
+        UP = 1
 
         def dfs(i,j, heading):
 
-            visited[i][j] = True
-            ans.append(matrix[i][j])
+            if i<n and i>=0 and j<m and j>=0 and matrix[i][j] != '$':
+                ans.append(matrix[i][j])
+                matrix[i][j] = '$'
 
-            # exit
-            if heading == UP:
-                if i-1 >= 0 and visited[i-1][j] == 0:
-                    dfs(i-1, j, UP)
-                elif j+1 <= m-1 and visited[i][j+1] == 0: # RIGHT
-                    dfs(i, j+1, RIGHT)
-                else:
-                    return
-            elif heading == RIGHT:
-                if j+1 <= m-1 and visited[i][j+1] == 0:
-                    dfs(i, j+1, RIGHT)
-                elif i+1 <= n-1 and visited[i+1][j] == 0: # DOWN
-                    dfs(i+1, j, DOWN)
-                else:
-                    return
-            elif heading == DOWN:
-                if i+1 <= n-1 and visited[i+1][j] == 0:
-                    dfs(i+1, j, DOWN)
-                elif j-1 >= 0 and visited[i][j-1] == 0: # LEFT
-                    dfs(i, j-1, LEFT)
-                else:
-                    return
-            elif heading == LEFT:
-                if j-1 >= 0 and visited[i][j-1] == 0:
-                    dfs(i, j-1, LEFT)
-                elif i-1 >= 0 and visited[i-1][j] == 0: # UP
-                    dfs(i-1, j, UP)
-                else:
-                    return
+                if j+1>=i:
+                    dfs(i, j+1, UP)
+                dfs(i+1,j,0)
+                dfs(i,j-1,0)
+                dfs(i-1,j,0)
 
 
-        dfs(0,0, RIGHT)
+        dfs(0,0, 0)
 
         return ans
 '''
