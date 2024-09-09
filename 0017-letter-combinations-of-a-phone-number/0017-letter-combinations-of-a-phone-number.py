5619842719d1c5192be432557e6fd35d2a70b9ae
daely178 <70620782+daely178@users.noreply.github.com>
2024-09-09 00:05:57 -0700

    Time: 44 ms (6.87%), Space: 16.6 MB (50.1%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,21 +1,28 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
+
         if not digits:
-            return []
-            
-        letters = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", 
-                   "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}        
+            return
+        
+        letters = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
 
         res = []
 
-        def backtrack(combi, digits):
-            if not digits:
+        def backtrack(combi, digit):
+            if not digit:
                 res.append(combi)
                 return
-            
-            for letter in letters[digits[0]]:
-                backtrack(combi+letter, digits[1:])
-        
+            for c in letters[digit[0]]:
+                backtrack(combi+c, digit[1:])
+
+                
         backtrack("", digits)
+
         return res
+
+'''
+digits = "23"
+["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+combi = "a"
+'''        
