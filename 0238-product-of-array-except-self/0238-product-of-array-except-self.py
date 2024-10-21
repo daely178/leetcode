21ffa58c414ea24b9bec1061302f5542b9a6f65d
daely178 <70620782+daely178@users.noreply.github.com>
2024-10-21 15:12:52 -0700

    Time: 18 ms (99.61%), Space: 22.2 MB (99.84%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,29 +1,31 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        ans = [0]*len(nums)
-        zero_cnt = 0
-        zero_pos = 0
+
         product_all = 1
-
-        # More than 2 zero : return all zero
-        # 1 zero : Find zero position and put calculated value for zero position in result array
-        # Non zero : divide product all by each num
-
-        for id, val in enumerate(nums):
-            if val == 0:
-                zero_cnt += 1
-
-                if zero_cnt > 1:
-                    return ans
-                zero_pos = id
+        zero_cnt = 0
+        zero_id = 0
+        ans = [0]*len(nums)
+        for idx, num in enumerate(nums):
+            if num:
+                product_all*=num
             else:
-                product_all *= val
-
-        if zero_cnt==1:
+                zero_cnt+=1
+                if zero_cnt>1:
+                    return ans
+                zero_pos = idx
+        if zero_cnt:
             ans[zero_pos] = product_all
             return ans
-        
-        for index in range(len(nums)):
-            ans[index] = product_all//nums[index]
-
+        for i in range(len(nums)):
+            ans[i] = product_all//nums[i]
         return ans
+'''
+    one zero
+    two or more zeroes
+    same value
+    
+    count zero and remember index
+    if more than one zero, return all zero
+    product all except zero
+    divide by its num to get product all except nums[i]
+'''
