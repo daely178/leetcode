eb89dfff4a86526d7bf005ce8c879b4fdc9a61d3
daely178 <70620782+daely178@users.noreply.github.com>
2024-10-21 16:09:22 -0700

    Time: 7 ms (99.54%), Space: 17.4 MB (11.22%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,29 +1,28 @@
 class Solution:
-    def maxProduct2(self, nums: List[int]) -> int:
+    def maxProduct(self, nums: List[int]) -> int:
+
         if len(nums) == 0:
             return 0
-
-        result = nums[0]
-
-        for i in range(len(nums)):
-            accu = 1
-            for j in range(i, len(nums)):
-                accu *= nums[j]
-                result = max(result, accu)
-
-        return result        
-
-    def maxProduct(self, nums: List[int]) -> int:
-        curMax, curMin = 1, 1
-        res = nums[0]
         
-        for n in nums:
-            vals = (n, n * curMax, n * curMin)
-            curMax, curMin = max(vals), min(vals)
-			
-            res = max(res, curMax)
-            
-        return res    
+        curmin, curmax = nums[0],nums[0]
+        res = curmax
+        for num in nums[1:]:
+            vals = (num, curmin*num, curmax*num)
+            curmin, curmax = min(vals), max(vals)
+            res = max(res, curmax)
+        return res
 '''
-    nums = [2,3,-2,4]
+    rough idea
+    for loops and find max for each one
+
+    for num in nums:
+        prod = 1
+        for num2 in nums[1:]:
+            prod*=num2
+            ans = max(ans, prod)
+    return ans
+
+    or 
+    maintain min max for negative value
+
 '''        
