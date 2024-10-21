798fccfc11cd9e1710d61cdf14d251fda05d7745
daely178 <70620782+daely178@users.noreply.github.com>
2024-10-20 22:40:19 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -3,18 +3,15 @@ class Solution:
         
         if len(intervals) == 0:
             return 0
-
-        intervals.sort(key=lambda x:x[0])
+        intervals.sort(key=lambda x:x[1])
 
         mh = []
-        heapq.heappush(mh, intervals[0][1])
-
-        for interval in intervals[1:]:
-            if mh[0] <= interval[0]:
+        for interval in intervals:
+            if mh and mh[0] <= interval[0]:
                 heapq.heappop(mh)
 
             heapq.heappush(mh, interval[1])
-        
+
         return len(mh)
 
 '''
@@ -23,10 +20,13 @@ class Solution:
         [5,10],              <- room 2
                  [15,20]
 
-    
+    [2. 4]
+            [7. 10]
 
-    q [ 30, 10, ]
+    q [ 10, 30, ]
     if 30 <= 5
 
-    
+    [[2,    11],
+        [6,     16],
+           [11,   16]]
 '''        
