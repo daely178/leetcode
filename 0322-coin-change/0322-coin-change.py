54732aaf89d9cd37ad26963c4140882f805233c6
daely178 <70620782+daely178@users.noreply.github.com>
2024-10-23 21:54:14 -0700

    Time: 701 ms (90.79%), Space: 17.1 MB (37.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,34 +1,16 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [0] + [amount + 1] * amount
 
-        @lru_cache(None)
-        def dfs(remaining):
-            if remaining < 0:
-                return -1
-            if remaining == 0:
-                return 0
-            min_cost = float('inf')
-            for coin in coins:
-                res = dfs(remaining-coin)
-                if res != -1:
-                    min_cost = min(min_cost, res+1)
-            return min_cost if min_cost != float('inf') else -1
-        return dfs(amount)
-                    
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        return -1 if dp[amount] == amount + 1 else dp[amount]
 
 '''
-    brute force 
-    deduct coin if coin <= amount
-    amount - coin
-    then next coin
+    [1,2,5], amount = 11
+    1-10, 1-9
 
-    optimize
-    coins = [1,2,5], amount = 11
-
-                    11
-          1           2           5
-     1    2.   5. 1   2.   5.  1. 2.  5
- 1 2 5    
-
-    recursive for loop to choose all coins
+    select 1 - DFS
 '''
