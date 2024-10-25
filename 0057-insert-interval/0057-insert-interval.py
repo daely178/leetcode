afda9de18aa63451a7fc493ed13d2aef4f2e21cf
daely178 <70620782+daely178@users.noreply.github.com>
2024-10-24 23:53:10 -0700

    Time: 1 ms (96.36%), Space: 18.8 MB (93.42%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,37 +1,14 @@
 class Solution:
-    def insert2(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         intervals.append(newInterval)
         intervals.sort()
-
         merged = [intervals[0]]
         for interval in intervals[1:]:
             if merged[-1][1] < interval[0]:
                 merged.append(interval)
             else:
-                merged[-1][1] = max(interval[1], merged[-1][1])
-        
+                merged[-1][1] = max(merged[-1][1], interval[1])
         return merged
-                
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        merged = []
-
-        for interval in intervals:
-            # case 1
-            if interval[1] < newInterval[0]:
-                merged.append(interval)
-            elif interval[0] > newInterval[1]:
-                merged.append(newInterval)
-                newInterval = interval
-            else:
-                if interval[1] >= newInterval[0] or interval[0] < newInterval[1]:
-                    newInterval[0] = min(interval[0], newInterval[0])
-                    newInterval[1] = max(interval[1], newInterval[1])
-
-        merged.append(newInterval)
-
-        return merged                
-        
             
 '''
 intervals [[1,2],[3,5],[6,10],[12,16]]
