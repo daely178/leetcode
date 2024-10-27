6c9a1c08b579db0cdc43017d2b0eb17bf6ded26c
daely178 <70620782+daely178@users.noreply.github.com>
2024-10-26 17:54:23 -0700

    Time: 0 ms (100%), Space: 16.6 MB (8.96%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+class Solution2:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         
         if not root:
@@ -17,21 +17,20 @@ class Solution:
 
         return root
 
-class Solution2:
+class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:     
 
         if not root:
             return None
 
-        queue = [root]
+        queue = collections.deque([root])
         while queue:
-            curr = queue.pop()
+            curr = queue.popleft()
             curr.left, curr.right = curr.right, curr.left
             if curr.left:
                 queue.append(curr.left)
             if curr.right:
-                queue.append(curr.right)            
-            
+                queue.append(curr.right)                        
 
         return root   
 
@@ -39,13 +38,5 @@ class Solution2:
 '''
 1. recursive
 2. iterative
-   queue or deque
-   stack
-
-#[4,2,7,1,3,6,9]
-# stack = 4 - 2 - 1 - None - None(1.right) - 3 None None - 
-# 1.left = None, 1.right = None return 1
-# 3.left = None, 3.right = None return 3
-# 2.left = 3 2.right = 1
-
+    q
 '''        
