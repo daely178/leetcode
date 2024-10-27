bae10084d027258b8e508b7125c29ec80e723d0b
daely178 <70620782+daely178@users.noreply.github.com>
2024-10-26 22:29:24 -0700

    Time: 3 ms (35.7%), Space: 18.8 MB (10.25%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        def levelorder(node, low=-math.inf, high=math.inf):
+            if not node:
+                return True
+            if node.val <= low or node.val >= high:
+                return False
+            return levelorder(node.right, node.val, high) and levelorder(node.left, low, node.val)
+        
+        return levelorder(root)
+
+'''
+    left val < root val < right val
+    recursive vs iteration
+
+                1
+            2        3
+        4.     5 6       7
+
+    inorder - left - root - right - recursive
+        4-2-5-1-6-3-7
+    preorder - root - left - right - recursive
+        1-2-4-5-3-6-7
+    postorder - left - right -root - recursive
+        4-5-2-6-7-3-1
+    levelorder - root - left - right with Q
+        1-2-3-4-5-6-7
+'''
