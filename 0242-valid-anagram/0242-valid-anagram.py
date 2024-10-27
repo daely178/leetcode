0216c0bf777e09239f79cbad89fc15041d6c1f69
daely178 <70620782+daely178@users.noreply.github.com>
2024-10-27 01:51:41 -0700

    Time: 11 ms (76.55%), Space: 16.7 MB (92.4%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        freq_s = defaultdict(int)
+        freq_t = defaultdict(int)
+        for c in s:
+            freq_s[c] += 1
+        for c in t:
+            freq_t[c] += 1
+        
+        return freq_s == freq_t
