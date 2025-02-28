ac26b511bc935e6809af222c8165e392f1218aa6
daely178 <70620782+daely178@users.noreply.github.com>
2025-02-28 00:11:22 -0800

    Time: 83 ms (57.7%), Space: 177.7 MB (51.6%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,68 @@
+struct Node {
+    int key;
+    int val;
+    Node* prev;
+    Node* next;
+    Node(int key, int val) : key(key), val(val), next(nullptr), prev(nullptr) {}
+};
+
+class LRUCache {
+public:
+    int capacity;
+    unordered_map<int, Node*> dic;
+    Node *head = new Node(-1, -1);
+    Node *tail = new Node(-1, -1);
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        if(dic.find(key) == dic.end()) {
+            return -1;
+        }
+        Node *node = dic[key];
+        remove(node);
+        add(node);
+        return node->val;
+    }
+    
+    void put(int key, int value) {
+        if(dic.find(key) != dic.end()){
+            Node *old = dic[key];
+            remove(old);
+        }
+
+        Node *node = new Node(key, value);
+        dic[key] = node;
+        add(node);
+
+        if(dic.size() > capacity) {
+            Node *nodeToDel = head->next;
+            remove(nodeToDel);
+            dic.erase(nodeToDel->key);
+        }
+    }
+
+    void add(Node* node)
+    {
+        Node *prevEnd = tail->prev;
+        prevEnd->next = node;
+        node->prev = prevEnd;
+        node->next = tail;
+        tail->prev = node;
+    }
+
+    void remove(Node* node){
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
