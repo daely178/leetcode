b4fc6d7937d0d03f546ef3f8eeda0728c805de4a
daely178 <70620782+daely178@users.noreply.github.com>
2025-03-08 16:05:08 -0800

    Time: 0 ms (100%), Space: 9.1 MB (86.86%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<uint32_t> dp(target+1, 0);
-        dp[0]=1;
-
-        for(int i=1; i<=target; i++) {
-            for(int num : nums) {
-                if(i-num>=0){
-                    dp[i]+=dp[i-num];
-                }
+        memo.clear();
+        return cal(nums, target);
+    }
+private:
+    unordered_map<int,int> memo;
+    int cal(vector<int>& nums, int target) {
+        if(target == 0){
+            return 1;
+        }
+        if(memo.find(target)!=memo.end()){
+            return memo[target];
+        }
+        int result=0;
+        for(int num : nums){
+            if(target-num>=0){
+                result += cal(nums, target-num);
             }
         }
-        return dp[target];
+        memo[target] = result;
+        return result;
     }
 };
+
+/*
+    1,2,3
+    4
+    1 0 0 0
+      
+
+    1       2       3
+1   2      1. 2    1
+1. 1    1
+1
+*/
