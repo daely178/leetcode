03d74965886312f8e8df014c1d746372747c64ab
daely178 <70620782+daely178@users.noreply.github.com>
2025-03-08 22:18:52 -0800

    Time: 23 ms (68.16%), Space: 17.8 MB (91.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> dp(amount+1, -1);
         dp[0] = 0;
 
-        for(int i=0; i<=amount; i++) {
+        for(int i=1; i<=amount; i++) {
             for(int c : coins) {
                 if(i-c>=0 && dp[i-c]!= -1){
                     dp[i] = dp[i]>0? min(dp[i], dp[i-c]+1) : dp[i-c]+1;
