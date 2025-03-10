82e4d87c4a6f035874a9e25607476bc9945eab03
daely178 <70620782+daely178@users.noreply.github.com>
2025-03-10 01:10:56 -0700

    Time: 1 ms (53.92%), Space: 16.9 MB (47.97%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.cpp
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
-        if(edges.size() != n-1) return false;
         vector<vector<int>> adjList(n);
-        for(auto e : edges) {
+
+        if(edges.size() != n-1){
+            return false;
+        }
+
+        for(const auto &e : edges){
             adjList[e[0]].push_back(e[1]);
             adjList[e[1]].push_back(e[0]);
         }
+
         vector<bool> visited(n, false);
         stack<int> st;
         st.push(0);
-        while(!st.empty()) {
+
+        while(!st.empty()){
             int node = st.top();
             st.pop();
             if(visited[node] == false){
                 visited[node] = true;
-                for(auto nei : adjList[node]){
-                    st.push(nei);
+                for (int neigh : adjList[node]) {
+                    if (!visited[neigh])
+                        st.push(neigh);
                 }
             }
         }
-        for(bool v : visited) {
-            if(!v) return false;
+        for(bool v : visited){
+            if(!v) {
+                return false;
+            }
         }
         return true;
     }
