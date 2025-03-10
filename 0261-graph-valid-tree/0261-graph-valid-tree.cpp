c47608d338dd9c41a1a2f25df5fc828600c0ac71
daely178 <70620782+daely178@users.noreply.github.com>
2025-03-10 00:56:14 -0700

    Time: 0 ms (100%), Space: 16.6 MB (55.82%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.cpp
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
-        if (edges.size() != n-1) return false;
-
-        vector<vector<int>> dag(n);
-
-        for(auto &edge : edges) {
-            int e = edge[0], v = edge[1];
-            dag[e].push_back(v);
-            dag[v].push_back(e);
+        if(edges.size() != n-1) return false;
+        vector<vector<int>> adjList(n);
+        for(auto e : edges) {
+            adjList[e[0]].push_back(e[1]);
+            adjList[e[1]].push_back(e[0]);
         }
-        unordered_set<int> explored;
-        dfs(0, dag, explored);
-        return explored.size() == n;
-    }
-    void dfs(int cur, const vector<vector<int>> &dag, unordered_set<int> &explored) {
-        if(explored.count(cur)) return;
-        explored.insert(cur);
-
-        for(int adj : dag[cur]) {
-            dfs(adj, dag, explored);
+        vector<bool> visited(n, false);
+        stack<int> st;
+        st.push(0);
+        while(!st.empty()) {
+            int node = st.top();
+            st.pop();
+            if(visited[node] == false){
+                visited[node] = true;
+                for(auto nei : adjList[node]){
+                    st.push(nei);
+                }
+            }
         }
+        for(bool v : visited) {
+            if(!v) return false;
+        }
+        return true;
     }
 };
