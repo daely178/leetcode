b4cb66a3baba8761d8be274332708b183f886dc5
daely178 <70620782+daely178@users.noreply.github.com>
2025-03-15 23:54:18 -0700

    Time: 61 ms (86.68%), Space: 172.7 MB (93.52%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,67 +1,64 @@
-struct Node {
+struct Node{
     int key;
-    int val;
-    Node* prev;
-    Node* next;
-    Node(int key, int val) : key(key), val(val), next(nullptr), prev(nullptr) {}
+    int value;
+    Node *prev;
+    Node *next;
+    Node(int key, int value) : key(key), value(value), prev(nullptr), next(nullptr){}
 };
-
 class LRUCache {
 public:
     int capacity;
-    unordered_map<int, Node*> dic;
-    Node *head = new Node(-1, -1);
-    Node *tail = new Node(-1, -1);
-    LRUCache(int capacity) {
+    unordered_map<int, Node*> mp;
+    Node *head = new Node(-1,-1);
+    Node *tail = new Node(-1,-1);
+
+    LRUCache(int capacity){
         this->capacity = capacity;
-        head->next = tail;
+        head->next= tail;
         tail->prev = head;
     }
     
     int get(int key) {
-        if(dic.find(key) == dic.end()) {
+        if(mp.find(key) == mp.end()){
             return -1;
         }
-        Node *node = dic[key];
+        Node *node = mp[key];
         remove(node);
         add(node);
-        return node->val;
+        return node->value;
     }
     
     void put(int key, int value) {
         Node *old = nullptr;
-        if(dic.find(key) != dic.end()){
-            Node *old = dic[key];
+        if(mp.find(key) != mp.end()){
+            old = mp[key];
             remove(old);
         }
-
-        if(old != nullptr){
-            old->val = value;
+        if(old != nullptr) {
             old->key = key;
+            old->value = value;
         }
         else {
             old = new Node(key, value);
         }
-        dic[key] = old;
+        mp[key] = old;
         add(old);
 
-        if(dic.size() > capacity) {
-            Node *nodeToDel = head->next;
-            remove(nodeToDel);
-            dic.erase(nodeToDel->key);
+        if(mp.size() > this->capacity) {
+            Node *delNode = head->next;
+            remove(delNode);
+            mp.erase(delNode->key);
         }
     }
 
-    void add(Node* node)
-    {
-        Node *prevEnd = tail->prev;
-        prevEnd->next = node;
-        node->prev = prevEnd;
+    void add(Node *node){
+        Node *last = tail->prev;
+        last->next = node;
+        node->prev = last;
         node->next = tail;
         tail->prev = node;
     }
-
-    void remove(Node* node){
+    void remove(Node *node) {
         node->prev->next = node->next;
         node->next->prev = node->prev;
     }
