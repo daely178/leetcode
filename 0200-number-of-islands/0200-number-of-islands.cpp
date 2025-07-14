52504dea89b485f4f3fe94e8d66cfc8c2b0c8621
daely178 <70620782+daely178@users.noreply.github.com>
2025-07-13 23:44:17 -0700

    Time: 22 ms (88.64%), Space: 16.1 MB (96.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int numOfIslands = 0;
-
+        int islands=0;
         for(int i=0; i<grid.size(); i++) {
-            for(int j=0; j<grid[0].size(); j++) {
+            for(int j=0; j<grid[0].size(); j++){
                 if(grid[i][j] == '1') {
-                    dfs(grid, i,j);
-                    numOfIslands++;
+                    dfs(i,j,grid);
+                    islands++;
                 }
             }
         }
-        return numOfIslands;
+        return islands;
     }
 
-    void dfs(vector<vector<char>> &grid, int i, int j){
-        if(i>=grid.size() || i<0 || j>=grid[0].size() || j<0 || grid[i][j] != '1') {
+    void dfs(int r, int c, vector<vector<char>>&grid) {
+        if(r<0 || r>=grid.size() || c <0 || c>=grid[0].size() || grid[r][c]!='1')
             return;
-        }
-
-        grid[i][j] = '0';
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
+        grid[r][c] = 'x';
+        dfs(r+1, c, grid);
+        dfs(r-1, c, grid);
+        dfs(r, c+1, grid);
+        dfs(r, c-1, grid);
     }
 };
+
+/*
+    looping and find the '1'
+    search connected 1's and mark 'x' not to visit again
+    make sure not to visit out of index
+    increase counter if there is no more
+*/
