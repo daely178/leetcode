734f5c5065c7fe8433818c8ad8a5d5d3f79c17ba
daely178 <70620782+daely178@users.noreply.github.com>
2025-07-13 23:27:36 -0700

    Time: 0 ms (100%), Space: 26.1 MB (60.52%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left=0, right = height.size()-1;
+        int left=0, right=height.size()-1;
         int maxL=height[left], maxR=height[right];
-        int res = 0;
+
+        int res=0;
 
         while(left<right) {
             if(maxL <= maxR) {
@@ -11,13 +12,29 @@ public:
                 maxL = max(maxL, height[left]);
                 res += (maxL-height[left]);
             } else {
-                right -= 1;
+                right-=1;
                 maxR = max(maxR, height[right]);
                 res += (maxR-height[right]);
             }
         }
         return res;
-
-        return res;
     }
 };
+
+/*
+
+        [0,1,0,2,1,0,1,3,2,1,2,1]
+maxL.    0 0 1 1 2 2 2 2 3 3 3 3    
+maxR.    3 3 3 3 3 3 3 2 2 2 1 0  
+min(L,R) 0 0 1 1 2 2 2 2 2 2 1 0
+height(i)- min(L,R)
+         0 0 1 1 0 2 1 0 0 1 0 0
+
+
+        [0,1,0,2,1,0,1,3,2,1,2,1]
+
+         0 1 
+max l                  3 
+max r                    2             
+res      0 0 1 0 1 2 1 0 0 1 0 0 
+*/
