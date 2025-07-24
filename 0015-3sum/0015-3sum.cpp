a887291d0afa5b94cb34e5e04f0e8256405416d4
daely178 <70620782+daely178@users.noreply.github.com>
2025-07-23 22:22:55 -0700

    Time: 1234 ms (8.65%), Space: 291.7 MB (10.37%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> temp;
+        if(nums.size() < 3)
+            return {};
+        
+        set<std::vector<int>> res;
         sort(nums.begin(), nums.end());
-
-        for(int i=0; i<nums.size()-2; i++) {
-            int l=i+1, r=nums.size()-1;
-
+        for(int i=0; i<nums.size(); i++) {
+            int l=i+1;
+            int r=nums.size()-1;
             while(l<r) {
                 int target = nums[i] + nums[l] + nums[r];
-                if(target == 0){
-                    temp.insert({nums[i], nums[l], nums[r]});
+                if(target > 0)
+                    r-=1;
+                else if(target < 0)
+                    l+=1;
+                else {
+                    res.insert({nums[i], nums[l], nums[r]});
                     l++;
                     r--;
                 }
-                else if(target < 0)
-                    l++;
-                else
-                    r--;
-            }
+            }            
         }
-        return vector<vector<int>>(temp.begin(), temp.end());
+        return vector<vector<int>>(res.begin(), res.end());
     }
 };
+
+/*
+    nums[i] + nums[j] + nums[k] = 0
+    i!=j!=k
+
+    brute force
+    for i=0 i<n
+        for j=i+1 j<n-1;
+            for k=j+1 k<n-2;
+*/
