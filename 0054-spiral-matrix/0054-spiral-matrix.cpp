2a33cf3c536a7e928b5dc3f8db44cd1498721356
daely178 <70620782+daely178@users.noreply.github.com>
2025-07-28 23:38:38 -0700

    Time: 0 ms (100%), Space: 9.3 MB (79.36%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,41 +1,52 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        int up=0, left=0, right=cols-1, down=rows-1;
+        int LEFT=0, RIGHT=matrix[0].size()-1;
+        int UP=0, DOWN=matrix.size()-1;
+        vector<int> res;
 
-        /*
-                  up
-            left     right
-                 down
-            
-            matrix[row][col]
-        */
+        while(LEFT<=RIGHT && UP<=DOWN) {
+            int i=0;
+            // go RIGHT
+            i=LEFT;
+            while(i<=RIGHT){
+                res.push_back(matrix[UP][i++]); // 1,2 // UP = 1
+            }            
+            UP++;
+            if(UP > DOWN)
+                break;
 
-        while(ans.size() < rows*cols) {
-            for(int col=left; col<=right; col++){
-                ans.push_back(matrix[up][col]);
+            // go DOWN
+            i=UP;
+            while(i<=DOWN){
+                res.push_back(matrix[i++][RIGHT]);
             }
-            for(int row=up+1; row<=down; row++){
-                ans.push_back(matrix[row][right]);
+            --RIGHT;            
+            if(LEFT > RIGHT)
+                break;
+
+            // go LEFT
+            i=RIGHT;
+            while(i>=LEFT){
+                res.push_back(matrix[DOWN][i--]);
             }
-            if(up!=down) {
-                for(int col=right-1; col>=left; col--){
-                    ans.push_back(matrix[down][col]);
-                }
-            }
-            if(left!=right){
-                for(int row=down-1; row>up; row--){
-                    ans.push_back(matrix[row][left]);
-                }
-            }
-            left++;
-            right--;
-            up++;
-            down--;
+            --DOWN;
+            if(UP > DOWN)
+                break;
+            // go UP
+            i=DOWN;
+            while(i>=UP){
+                res.push_back(matrix[i--][LEFT]);
+            }            
+            LEFT++;
         }
-        return ans;
+
+        return res;
     }
 };
+
+/*
+    RIGHT DOWN LEFT UP
+
+    for()
+*/
