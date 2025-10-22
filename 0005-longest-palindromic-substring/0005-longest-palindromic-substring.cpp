262b6da3ed4bb79f173aa636546c93be40f70b5f
daely178 <70620782+daely178@users.noreply.github.com>
2025-10-22 07:51:57 -0700

    Time: 19 ms (52.36%), Space: 22 MB (39.69%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int m = s.size();
         int maxlen = 0;
-        string res="";
-        for(int i=0; i<s.size(); i++) {            
+        string res = "";
+        int l,r;
 
-            // odd case
-            // babad
-            int l=i, r=i;
-            while(l>=0 && r<m && s[l]==s[r]) {
+        for(int i=0; i<s.size(); i++) {
+            l=i;
+            r=i;
+            while(l>=0 && r<s.size() && s[l]==s[r]) {
                 if(maxlen < (r-l+1)) {
+                    res = s.substr(l, r-l+1);
                     maxlen = r-l+1;
-                    res = s.substr(l,r-l+1);
                 }
                 l--;
                 r++;
-            }         
-            l=i, r=i+1;
-            while(l>=0 && r<m && s[l]==s[r]) {
+            }
+            l=i;
+            r=i+1;
+            while(l>=0 && r<s.size() && s[l]==s[r]) {
                 if(maxlen < (r-l+1)) {
+                    res = s.substr(l, r-l+1);
                     maxlen = r-l+1;
-                    res = s.substr(l,r-l+1);
                 }
                 l--;
                 r++;
             }            
         }
+
         return res;
     }
 };
+
+/*
+    babad
+ l          r
+      l
+      r
+
+    cbbd
+    l
+    r
+*/
