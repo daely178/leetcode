d36910e8c8b311811f94309b1f4422872b0ab844
daely178 <70620782+daely178@users.noreply.github.com>
2025-10-23 19:42:55 -0700

    Time: 11 ms (12.89%), Space: 12.6 MB (9.74%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int> dic;
-        for(char c : magazine){
-            dic[c]++;
+        unordered_map<char, int> mp = {};
+        for(int i=0; i<ransomNote.size(); i++){
+            mp[ransomNote[i]]++;
         }
-        
-        for(char c : ransomNote){
-            if(dic.find(c) != dic.end() && dic[c] >0){
-                dic[c]--;
+        for(int i=0; i<magazine.size(); i++) {
+            if(mp.find(magazine[i]) != mp.end() && mp[magazine[i]] > 0) {
+                mp[magazine[i]]--;
             }
-            else {
+        }
+        for(int i=0; i<mp.size(); i++) {
+            if(mp[i]) {
                 return false;
             }
         }
