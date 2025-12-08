c4a7110c6fa1fa85edf675ea775d57855f10f49d
daely178 <70620782+daely178@users.noreply.github.com>
2025-12-07 20:30:11 -0800

    Time: 63 ms (82.2%), Space: 177.8 MB (49.58%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,6 +1,6 @@
-struct Node {
-    int value;
+struct Node{
     int key;
+    int value;
     Node *prev;
     Node *next;
     Node(int key=0, int value=0) : key(key), value(value), prev(nullptr), next(nullptr) {}
@@ -8,13 +8,8 @@ struct Node {
 
 class LRUCache {
 public:
-    unordered_map<int, Node*> mp;
-    int capacity;
-    Node *head = new Node();
-    Node *tail = new Node();
-
     LRUCache(int capacity) {
-        this->capacity = capacity;
+        this->capacity_ = capacity;
         head->next = tail;
         tail->prev =head;
     }
@@ -30,20 +25,20 @@ public:
         return tmp->value;
     }
     
-    void put(int key, int value) {
+    void put(int key, int value) {        
         if(mp.find(key) != mp.end()) {
             Node *old = mp[key];
             remove(old);
         }
-        Node *node = new Node(key, value);
-        add(node);
-        mp[key] = node;
+        Node *tmp = new Node(key, value);
+        mp[key] = tmp;
+        add(tmp);
 
-        if(mp.size() > this->capacity) {
+        if(mp.size() > capacity_) {
             Node *first = head->next;
             remove(first);
             mp.erase(first->key);
-        }
+        }        
     }
 
     void add(Node *node) {
@@ -51,13 +46,18 @@ public:
         last->next = node;
         node->prev = last;
         node->next = tail;
-        tail->prev = node;
+        tail->prev = node;        
     }
 
     void remove(Node *node) {
         node->prev->next = node->next;
         node->next->prev = node->prev;
     }
+private:
+    int capacity_;
+    std::unordered_map<int, Node*> mp;
+    Node *head = new Node();
+    Node *tail = new Node();
 };
 
 /**
@@ -65,10 +65,23 @@ public:
  * LRUCache* obj = new LRUCache(capacity);
  * int param_1 = obj->get(key);
  * obj->put(key,value);
+ */
 
- stack FIFO push(), pop() O(1)
- queue LIFO front(), pop(), push() O(1)
- Linked list
 
-  2 1 3
+ /*
+
+get() 
+return existing value for the key
+make it latest
+
+put()
+insert value if key doesn't exist
+update value if key exist
+make it the latest
+
+0(1)
+
+Find key should be using map()
+double linked list to insert/remove/add to manage items with O(1)
+
  */
