fe01879f9eff0c98e78c1c415a7335232a8fe447
daely178 <70620782+daely178@users.noreply.github.com>
2025-12-07 20:50:44 -0800

    Time: 0 ms (100%), Space: 97.4 MB (60.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if(prices.size() <= 1){
+        if(prices.empty()) {
             return 0;
         }
-
-        int buy = prices[0];
-        int maxP = 0;
+        int buy=prices[0];
+        int profit=0;
         for(int i=1; i<prices.size(); i++) {
             int sell = prices[i];
-
             if(sell > buy) {
-                maxP = max(maxP, sell-buy);
+                profit = max(profit, sell-buy);
             } else {
                 buy = sell;
             }            
         }
-        return maxP;
+        return profit;
     }
 };
+
+
+/*
+
+buy stock on ith day
+sell stock on the different day in the future
+
+find the max profit
+cannot acheive 0 profit
+
+buy = prices[0]
+for loop
+if sell > buy
+    profit = max(sell - buy, profit)
+    update profit until sell <= buy
+else
+    buy = sell
+
+
+[7,1,5,3,6,4]
+1=buy
+4=sell
+profit = 5
+
+[7,6,4,3,1]
+1=buy
+1=sell
+
+
+*/
