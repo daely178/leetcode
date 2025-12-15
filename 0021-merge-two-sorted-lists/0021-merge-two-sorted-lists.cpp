7f090d36f324193ef648e988bedd908d1191f3df
daely178 <70620782+daely178@users.noreply.github.com>
2025-12-14 17:05:25 -0800

    Time: 0 ms (100%), Space: 19.4 MB (86.33%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,23 +11,54 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
+        ListNode dummy;
+        ListNode *tmp = &dummy;
+
         ListNode* l1 = list1;
         ListNode* l2 = list2;
-        ListNode dummy(-1);
-        ListNode* l3 = &dummy;
 
         while(l1 && l2) {
-            if(l1->val > l2->val) {
-                l3->next = l2;
-                l2=l2->next;
+            if(l1->val < l2->val) {
+                tmp->next = l1;
+                l1 = l1->next;
             } else {
-                l3->next = l1;
-                l1=l1->next;
+                tmp->next = l2;
+                l2 = l2->next;
             }
-            l3=l3->next;
+            tmp = tmp->next;
         }
-        l3->next = l1 ? l1 : l2;
+        while(l1) {
+            tmp->next = l1;
+            l1=l1->next;
+            tmp = tmp->next;
+        }
+        while(l2) {
+            tmp->next = l2;
+            l2 = l2->next;
+            tmp = tmp->next;
+        }
+
         return dummy.next;
     }
 };
+
+/*
+
+dummy
+
+while(l1 && l2) 
+    if l1.val < l2.val
+        dummy.next = l1
+        l1 = l1.next
+    else
+        dummy.next = l2
+        l2 = l2.next
+
+while(l1)
+    dummy.next = l1
+    l1 = l1.next
+while(l2)
+    dummy.next = l2
+    le = l2.next
+
+*/
