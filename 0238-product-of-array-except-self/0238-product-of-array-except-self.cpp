aa3dc03c10ea437315e1483454574bc1ef2ff09f
daely178 <70620782+daely178@users.noreply.github.com>
2026-03-18 22:18:28 -0700

    Time: 3 ms (37.09%), Space: 41 MB (41.11%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> left(nums.size());
-        vector<int> right(nums.size());
+        int n = nums.size();
+        int left[n];
+        int right[n];
 
         left[0]=1;
         for(int i=1; i<nums.size(); i++) {
-            left[i] = left[i-1]*nums[i-1];
+            left[i] = nums[i-1]*left[i-1];
         }
-        right[nums.size()-1]=1;
+        right[nums.size()-1] = 1;
         for(int i=nums.size()-2; i>=0; i--) {
-            right[i] = right[i+1]*nums[i+1];
+            right[i] = nums[i+1]*right[i+1];
         }
 
         vector<int> ret(nums.size());
         for(int i=0; i<nums.size(); i++) {
             ret[i] = left[i]*right[i];
         }
+
         return ret;
     }
 };
@@ -34,4 +36,14 @@ left = [1,]
 left[i] = left[i-1]*nums[i-1]
 right = [1,]
 right[i] = right[r+1]*nums[i+1]
+
+
+left = [1,]
+left[i] = left[i-1]*nums[i-1]
+
+right = 1
+for(i=size-1; i>=0 i--) {
+    left[i] = left[i] * right
+    right *= nums[i] // right will have all product of i's right
+}
 */
