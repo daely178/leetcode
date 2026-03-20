543fa2bc789dccd09766fbbefef9a259b468fef2
daely178 <70620782+daely178@users.noreply.github.com>
2026-03-19 21:11:04 -0700

    Time: 14 ms (39.84%), Space: 11.8 MB (64.68%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.empty()) {
-            return 0;
-        }
-        int l=0;
-        int res = 0;
-        int n = s.size();
-        std::unordered_map<char, int> mp;
+        int l=0,res=0;
+        unordered_map<char, int> mp;
 
-        for(int r=0; r<n; r++) {
-            if(mp.find(s[r]) != mp.end()){
-                l = max(l, mp[s[r]]+1);
+        for(int r=0; r<s.size(); r++) {
+            if(mp.find(s[r])!=mp.end()) {
+                l = max(mp[s[r]]+1, l);                
             }
-            mp[s[r]] = r;
             res = max(res, r-l+1);
+            mp[s[r]] = r;
         }
+
         return res;
     }
 };
 
 
 /*
+longest substring without duplicate
 
-   a b c a b c b b
- l 1 2 3 4 5 6 7 8
- r             7
- m.1 2 3 3 3 3 3 3    
+
+
+  0 1 2 3 4 5 6 7
+  a b c a b c b b
+  l
+  r
+
+  mp[a] = 0
+  mp[b] = 1
+  mp[c] = 2
+
+  l = max(mp[s[r]], l)+1
+  mp[s[r]] = r
+
+  ans = max(ans, r-l+1)
+
 */
