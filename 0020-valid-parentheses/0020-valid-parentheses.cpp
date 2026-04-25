06413a784e3ad9b137f938712f957cade7b5cf89
daely178 <70620782+daely178@users.noreply.github.com>
2026-04-24 22:05:37 -0700

    Time: 0 ms (100%), Space: 8.7 MB (97.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> par;
 
-        for(auto c : s) {
-            if(c == '{' || c == '[' || c == '(')
-                par.push(c);
-            else {
-                if(!par.empty() && 
-                (c=='}' && par.top() == '{' ||
-                 c==']' && par.top() == '[' ||
-                 c==')' && par.top() == '('))
-                 par.pop();
-                else
+        std::stack<char> st;
+
+        for(const auto c : s) {
+            if(c == '{' || c == '[' || c == '(') {
+                st.push(c);
+            } else if(c == '}' || c == ']' || c == ')') {
+                if( !st.empty() &&
+                   ((c == '}' && st.top() == '{') ||
+                   (c == ']' && st.top() == '[') ||
+                   (c == ')' && st.top() == '(')) ) {
+                    st.pop();
+                } else {
                     return false;
+                }
             }
         }
-        return par.empty();
+        if(!st.empty()) {
+            return false;
+        }
+        return true;
     }
 };
+
+/*
+
+()[]{}
+
+if open, push to stack
+if close, check stack top
+
+*/
