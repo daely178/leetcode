6da419d18b2d6b1372cc6a890f3d8303581ffdde
daely178 <70620782+daely178@users.noreply.github.com>
2026-05-17 13:11:42 -0700

    Time: 0 ms (100%), Space: 15.8 MB (7.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,49 +2,35 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> mp;
-
         for(int i=0; i<nums.size(); i++) {
             mp[nums[i]] = i;
         }
-
         for(int i=0; i<nums.size(); i++) {
             int diff = target - nums[i];
-            if(mp.find(diff) !=  mp.end() && i!=mp[diff]) {
+            if(mp.find(diff) != mp.end() && i!=mp[diff]) {
                 return {i, mp[diff]};
             }
         }
-
-        return {};
+        return {-1,-1};
     }
 };
 
 
 /*
-do not use same element
-only one solution
-
 brute force
+for(i=0 i<n; i++)
+    for(j=1; j<n; j++)
+        if( target == (nums[i] + nums[j])) {
+            return {i,j};
+        }
 
-for i=0 i<n i++
-    for j=i+1 j<n j++
-        sum = nums[i] + nums[j]
-        if sum = target
-            return {i,j}
-
-return {};
-
-with additional memory
-
-unsorted_map<int, int> mp
-
-for i=0 i<n i++
-    mp[nums[i]] = i
-
-for i=0 i<n i++
-    diff = target - nums[i]
-    if(mp.find(diff) != mp.end()) {
+unordered_map<int,int> mp;
+for(i=0 i<n; i++)
+    mp[nums[i]] = i;
+for(i=0 i<n i++) {
+    int diff = target-nums[i]
+    if(mp.find(diff) != mp.end() && i!=nums[diff]) {
         return {i, mp[diff]}
     }
-
-return {};
+}
 */
