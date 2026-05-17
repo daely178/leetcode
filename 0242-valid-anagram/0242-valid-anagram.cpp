1e0bbadd2825fff9d05d29a9586775a81a415093
daely178 <70620782+daely178@users.noreply.github.com>
2026-05-17 13:37:25 -0700

    Time: 2 ms (49.8%), Space: 9.8 MB (48.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         unordered_map<char, int> freq;
-
-        for(auto c : s) {
-            freq[c]++;
+        for(int i=0; i<s.size(); i++) {
+            freq[s[i]]++;
         }
-        for(auto c : t) {
-            freq[c]--;
+        for(int i=0; i<t.size(); i++) {
+            if(freq.find(t[i]) == freq.end()) {
+                return false;
+            }
+            freq[t[i]]--;
+            if(freq[t[i]] < 0 ){
+                return false;
+            }
         }
-        for(auto f : freq) {
-            if(f.second != 0) {
+        for(const auto & pair : freq) {
+            if(pair.second != 0) {
                 return false;
             }
         }
         return true;
-
     }
 };
 
@@ -23,6 +27,9 @@ public:
 
 map string s
 
-if all count is zero in s map, return true
+brute force
+unordered_map<char, int> freq
+
+f
 
 */
