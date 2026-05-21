888626b73c18b0cf5793d56c9eb880081e204e64
daely178 <70620782+daely178@users.noreply.github.com>
2026-05-20 21:46:15 -0700

    Time: 24 ms (78.11%), Space: 16.3 MB (77.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
         int islands = 0;
-        for(int i=0; i<grid.size(); i++) {
-            for(int j=0; j<grid[0].size(); j++) {
-                if(grid[i][j] == '1'){
-                    helper(grid, i,j);
+
+        for(int r=0; r<rows; r++) {
+            for(int c=0; c<cols; c++) {
+                if(grid[r][c] == '1') {
+                    dfs(grid, r, c, rows, cols);
                     islands++;
                 }
             }
         }
         return islands;
     }
-
-    void helper(vector<vector<char>>& grid, int r, int c){
-        if(r>=grid.size() || r<0 || c >= grid[0].size() || c < 0 || grid[r][c] != '1') {
-            return;
+    void dfs(vector<vector<char>>& grid, int r, int c, int rows, int cols) {
+        if(c<cols && r<rows && c>=0 && r>=0 && grid[r][c]=='1') {
+            grid[r][c] = 'x';
+            dfs(grid, r+1, c, rows, cols);
+            dfs(grid, r-1, c, rows, cols);
+            dfs(grid, r, c+1, rows, cols);
+            dfs(grid, r, c-1, rows, cols);
         }
-        grid[r][c]='x';
-        helper(grid, r, c+1);
-        helper(grid, r, c-1);
-        helper(grid, r+1, c);
-        helper(grid, r-1, c);
     }
 };
 
@@ -29,5 +30,10 @@ public:
 
 search all corners and replace 1 with other char to show visited
 until helper returns
+
+dfs
+if "1"
+    dfs left,right,up,down and mark "x" for visited
+
 
 */
