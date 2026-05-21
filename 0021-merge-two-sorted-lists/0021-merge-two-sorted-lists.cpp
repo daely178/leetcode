4235859803ef5348e69a640435540e8e35e8dd12
daely178 <70620782+daely178@users.noreply.github.com>
2026-05-20 21:20:15 -0700

    Time: 0 ms (100%), Space: 19.5 MB (26.1%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,43 +11,24 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(!l1) {
-            return l2;
-        }
-        else if(!l2) {
-            return l1;
-        }
-        else if(l1->val < l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
-        }
-    }
-
-    ListNode* mergeTwoLists2(ListNode* list1, ListNode* list2) {
         ListNode dummy;
-        ListNode *tmp = &dummy;
-
-        ListNode* l1 = list1;
-        ListNode* l2 = list2;
+        ListNode *tail = &dummy;
 
         while(l1 && l2) {
             if(l1->val < l2->val) {
-                tmp->next = l1;
-                l1 = l1->next;
+                tail->next = l1;                
+                l1=l1->next;
             } else {
-                tmp->next = l2;
-                l2 = l2->next;
+                tail->next = l2;
+                l2=l2->next;
             }
-            tmp = tmp->next;
+            tail = tail->next;
         }
-
-        tmp->next = l1 ? l1 : l2;
+        tail->next = l1 ? l1 : l2;
 
         return dummy.next;
     }
+
 };
 
 /*
